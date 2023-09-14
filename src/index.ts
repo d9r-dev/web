201411f10b50d9bb5eeff8053bef33a30e9a226a
Danny Krämer <d.kraemer@dotsource.de>
2023-09-14 22:26:15 +0200

    adding views

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
-import { User } from './models/User';
+import { Collection } from './models/Collection';
+import { UserProps, User } from './models/User';
 
-const user = User.buildUser({ name: 'myname', age: 20 });
+const user = User.buildUser({ name: 'NAME', age: 20 });
+
+const collection = User.buildUserCollection();
+
+collection.fetch();
